c143c8fe96e48cd14607659606b4f32fe5a7685e
qiuzuhui <qiuzuhui@gmail.com>
2017-09-21 17:03:47 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <v-app light >
+    <div>
+      <v-alert success value="true">
+        This is a success alert.
+      </v-alert>
+    </div>
     <v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
@@ -10,7 +15,7 @@
       <v-list>
         <v-list-tile
           value="true"
-          v-for="(item, i) in items"
+          v-for="(item, i) in navItems"
           :key="i"
         >
           <v-list-tile-action>
@@ -24,32 +29,9 @@
     </v-navigation-drawer>
     <v-toolbar fixed class="light-blue">
       <v-toolbar-side-icon @click.stop="drawer = !drawer" dark></v-toolbar-side-icon>
-      <!--<v-btn-->
-        <!--icon-->
-        <!--light-->
-        <!--@click.stop="miniVariant = !miniVariant"-->
-      <!--&gt;-->
-        <!--<v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>-->
-      <!--</v-btn>-->
       <v-toolbar-title v-text="title" style="color:white;"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu
-        offset-y
-        :close-on-content-click="false"
-        :nudge-width="200"
-        v-model="menu"
-        style="margin-right: 20px;"
-      >
-        <v-avatar slot="activator">
-          <img src="/static/john.jpg" alt="John">
-        </v-avatar>
-        <v-list>
-          <v-list-tile >
-            <v-list-tile-title>LogOut</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
-
+      <user-menu></user-menu>
 
     </v-toolbar>
     <main>
@@ -76,16 +58,17 @@
 </template>
 
 <script>
-  import VMenu from '../node_modules/vuetify/src/components/VMenu/VMenu'
+  import UserMenu from './components/UserMenu'
+  import VAlert from 'vuetify/src/components/VAlert'
   export default {
-    components: {VMenu},
+    components: {VAlert, UserMenu},
     data () {
       return {
         clipped: true,
         drawer: true,
         fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
+        navItems: [
+          { icon: 'bubble_chart', title: '场景' }
         ],
         miniVariant: false,
         right: true,
---
+++ src/components/UserMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-menu
+    offset-y
+    :close-on-content-click="false"
+    :nudge-width="200"
+    class="mr-2 user-menu"
+  >
+    <div slot="activator" class="light-blue" >
+      <v-avatar size="40px">
+        <img src="/static/john.jpg" alt="John">
+      </v-avatar>
+      <div style="display: inline-block;color: white;padding: 5px;">John Leider</div>
+    </div>
+
+    <v-card>
+      <v-list>
+        <v-list-tile >
+          <v-list-tile-title>LogOut</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+  import VMenu from 'vuetify/src/components/VMenu/VMenu'
+  export default {
+    components: {VMenu},
+    name: 'UserMenu',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
