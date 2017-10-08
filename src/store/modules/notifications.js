49c804c27732427c5a5c9b700ef0170e8c441b86
qiuzuhui <qiuzuhui@gmail.com>
2017-10-09 00:59:36 +0800

    update

--- src/store/modules/notifications.js
+++ src/store/modules/notifications.js
@@ -9,11 +9,17 @@ export default {
   },
   mutations: {
     add (state, data) {
+      let id = Date.now()
       memory.add({
-        id: Date.now(),
+        id,
         type: data.type,
         msg: data.msg
       })
+      if (data.type !== 'error') {
+        setTimeout(() => {
+          this.commit('notifications/remove', id)
+        }, 3000)
+      }
     },
     remove (state, id) {
       memory.remove(id)
