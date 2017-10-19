96cf9d16b59f1d317c28ff62aeacf51ec1b8f04c
qiuzuhui <qiuzuhui@gmail.com>
2017-10-19 11:23:21 +0800

    update

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,9 +1,62 @@
 <template>
-  <div>
-    info
-  </div>
+  <v-container white elevation-5>
+    <v-layout row>
+      <v-flex xs12 lg6>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">用户名</v-flex>
+          <v-flex>{{currentUser.memberName}}</v-flex>
+        </v-layout>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">公司名称</v-flex>
+          <v-flex>{{currentUser.company}}</v-flex>
+        </v-layout>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">行业</v-flex>
+          <v-flex>{{currentUser.industry}}</v-flex>
+        </v-layout>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">等级</v-flex>
+          <v-flex>{{currentUser.level}}</v-flex>
+        </v-layout>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">当前余额</v-flex>
+          <v-flex>{{currentUser.totalAmount}}</v-flex>
+        </v-layout>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">冻结金额</v-flex>
+          <v-flex>{{currentUser.freezeAmount}}</v-flex>
+        </v-layout>
+        <v-layout class="info-field" row>
+          <v-flex class="info-label">当年消费总额</v-flex>
+          <v-flex>{{currentUser.consumptionAmount}}</v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
+  import { mapGetters } from 'vuex'
   export default {
+    data () {
+      return {}
+    },
+    computed: {
+      ...mapGetters({
+        currentUser: 'users/current'
+      })
+    }
   }
 </script>
+<style scoped>
+  .info-field {
+    padding: 5px 0;
+  }
+  .info-field .flex{
+    flex: 0 0 auto
+  }
+  .info-field .info-label.flex {
+    flex-basis: 150px;
+    font-weight: bolder;
+  }
+
+</style>
