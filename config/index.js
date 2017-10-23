c67189ee690c747b9a81a519be8a2fd850458944
qiuzuhui <qiuzuhui@gmail.com>
2017-10-23 13:42:33 +0800

    update

--- config/index.js
+++ config/index.js
@@ -7,6 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     login: path.resolve(__dirname, '../dist/login.html'),
     register: path.resolve(__dirname, '../dist/register.html'),
+    forget: path.resolve(__dirname, '../dist/forget.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
--- src/pages/ForgetPassword.vue
+++ src/pages/ForgetPassword.vue
@@ -1,16 +1,33 @@
 <template>
   <v-app light class="blue">
+    <div class="alert-list" :class="$vuetify.breakpoint.xsOnly? 'xs-list':''" row wrap align-center>
+      <transition-group name="alert-list">
+        <v-alert
+          v-for="item in notifications"
+          :class="item.type"
+          :key="item.id"
+          value="true"
+          dismissible
+          @input="removeNotification(item.id)"
+          transition="scale-transition"
+        >
+          {{item.msg}}
+        </v-alert>
+      </transition-group>
+    </div>
     <v-container grid-list-md>
       <div style="text-align: center;padding-top: 30px;">
         <h4 style="width: 100%;color: white;">找回密码</h4>
       </div>
-      <v-stepper v-model="paneState">
+      <v-stepper v-model="paneState" style="padding-bottom: 15px;">
         <v-stepper-header style="box-shadow: none;" class="pt-4">
           <v-stepper-step step="1" :complete="paneState > 1">账号信息</v-stepper-step>
           <v-divider></v-divider>
           <v-stepper-step step="2">安全验证</v-stepper-step>
           <v-divider></v-divider>
           <v-stepper-step step="3">密码重置</v-stepper-step>
+          <v-divider></v-divider>
+          <v-stepper-step step="4">设置完成</v-stepper-step>
         </v-stepper-header>
         <v-stepper-content step="1">
           <div style="width: 400px;margin: auto;">
@@ -38,14 +55,14 @@
                 <div class="title mb-2">为了您的账户安全，请进行安全验证</div>
                 <div class="subheading" style="color: gray;">验证方式: {{mailOrTel}}</div>
                 <div>
-                  <div style="display: inline-block;width: 250px;margin-right: 10px;">
+                  <div style="display: inline-block;width: 180px;margin-right: 10px;">
                     <v-text-field
                       label="验证码"
                       v-model="vCode"
                     ></v-text-field>
                   </div>
                   <v-btn style="display: inline-block;margin: 0;" :disabled="!!timer" @click="sendVerifyCode()">
-                    {{timer ? timer + '秒重试' : '发送验证码'}}
+                    {{timer ? '已发送，' + timer + '秒重试' : '发送验证码'}}
                   </v-btn>
                 </div>
                 <v-btn
@@ -89,6 +106,29 @@
             </v-card>
           </div>
         </v-stepper-content>
+        <v-stepper-content step="4">
+          <div style="width: 400px;margin: auto;">
+            <v-card flat>
+              <v-card-text>
+                <v-avatar style="display: inline-block;vertical-align: top;" size="60px">
+                  <v-icon class="green white--text" style="font-size: 59px;">check_circle</v-icon>
+                </v-avatar>
+                <div style="display: inline-block;padding: 0 10px;">
+                  <h4 style="margin: 0;">已完成!</h4>
+                  <div style="color: lightgray;text-align: left;">请牢记您设置的密码</div>
+                </div>
+                <v-btn
+                  style="margin-top: 15px;"
+                  @click.native.stop="home()"
+                  primary
+                  block
+                >
+                  登录
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </div>
+        </v-stepper-content>
       </v-stepper>
     </v-container>
   </v-app>
@@ -100,6 +140,7 @@
   import VSubheader from '../../node_modules/vuetify/src/components/VSubheader/VSubheader'
   import VBtn from '../../node_modules/vuetify/src/components/VBtn/VBtn'
   import api from '../api'
+  import { mapGetters } from 'vuex'
 
   export default {
     components: {
@@ -158,19 +199,60 @@
         })
       },
       resetPassword () {
-        return api.users.resetPassword(this.mailOrTel, this.token, this.user.password).then(function () {
-          location.href = 'index.html'
+        return api.users.resetPassword(this.mailOrTel, this.token, this.user.password).then(() => {
+          this.paneState = 4
         }).catch((err) => {
           this.$store.commit('notifications/add', {
             type: 'error',
             msg: err.message
           })
         })
+      },
+      removeNotification (id) {
+        this.$store.commit('notifications/remove', id)
+      },
+      home () {
+        location.href = 'index.html'
       }
+    },
+    computed: {
+      ...mapGetters({
+        notifications: 'notifications/all'
+      })
     }
   }
 
 </script>
-<style>
 
+
+<style>
+  .alert {
+    border: none !important;
+  }
+
+  .alert-list-enter-active, .alert-list-leave-active {
+    transition: all 1s;
+  }
+
+  .alert-list-enter, .alert-list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+
+  .alert-list {
+    position: fixed;
+    z-index: 999999;
+    width: 50%;
+    left: 25%;
+  }
+
+  .alert-list.xs-list {
+    width: 80%;
+    left: 10%;
+  }
+
+  .alert-list .alert .alert__dismissible > .icon,
+  .alert-list .alert .alert__icon.icon {
+    color: white;
+  }
 </style>
